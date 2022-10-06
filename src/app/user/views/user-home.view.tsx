300512edb7d2f0d88d783341c2c15b93263ea5df
Peter Akaliro <pin4eva@hotmail.com>
2022-10-06 13:48:25 +0100

    setup eslint

--- src/app/user/views/user-home.view.tsx
+++ src/app/user/views/user-home.view.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const UserHomeView = () => {
   return (
     <div>
-    <h1>UserHomeView</h1>
+      <h1>UserHomeView</h1>
     </div>
-  )
-}
+  );
+};
 
-export default UserHomeView
+export default UserHomeView;

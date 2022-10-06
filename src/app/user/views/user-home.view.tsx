d91cd70f5cd5ad0df7dc4c31fa9c6938276cc785
Peter Akaliro <pin4eva@hotmail.com>
2022-10-06 22:04:41 +0100

    removed styled-components

--- src/app/user/views/user-home.view.tsx
+++ src/app/user/views/user-home.view.tsx
@@ -2,8 +2,6 @@ const UserHomeView = () => {
   return (
     <div className="container">
       <h1>UserHomeView</h1>
-      <input type="text" className="form-control" placeholder="Date" />
-      <button className="btn btn-outline-secondary">Reset</button>
     </div>
   );
 };

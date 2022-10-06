458b1cafb96e09619ceaf7121fd7d56f69ea0b9c
Peter Akaliro <pin4eva@hotmail.com>
2022-10-06 14:01:23 +0100

    added bootstrap and setup directory base path in tsConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import AppRoute from "./app/index.routes";
+import "./global-styles/index.scss";
+import "./global-styles/custom.scss";
 
 const App = () => {
   return <AppRoute />;
